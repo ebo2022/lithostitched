f0b802bb783c3c7ed9bb513ead3da51859dffa7f
ebo2022 <99616031+ebo2022@users.noreply.github.com>
2025-12-22 20:13:50 -0500

    Normalizer vec function

---
+++ src/common/main/java/dev/worldgen/lithostitched/worldgen/vectorfunction/NormalizeVectorFunction.java
@@ -0,0 +1,19 @@
+package dev.worldgen.lithostitched.worldgen.vectorfunction;
+
+import com.mojang.serialization.MapCodec;
+import dev.worldgen.lithostitched.worldgen.LithostitchedCodecs;
+import net.minecraft.world.phys.Vec3;
+
+public record NormalizeVectorFunction(VectorFunction input) implements VectorFunction.PureTransformer, VectorFunction.SimpleFunction {
+    public static final MapCodec<NormalizeVectorFunction> CODEC = LithostitchedCodecs.singleArgument(VectorFunction.HOLDER_HELPER_CODEC, NormalizeVectorFunction::new, NormalizeVectorFunction::input);
+
+    @Override
+    public Vec3 transform(Vec3 input) {
+        return input.normalize();
+    }
+
+    @Override
+    public MapCodec<? extends VectorFunction> codec() {
+        return CODEC;
+    }
+}

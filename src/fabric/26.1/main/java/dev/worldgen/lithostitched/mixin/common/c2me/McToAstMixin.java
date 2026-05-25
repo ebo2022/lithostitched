e8e0572daeba1ef6d4a5c2320cf0ca149ef4b6e2
ebo2022 <99616031+ebo2022@users.noreply.github.com>
2026-05-25 10:12:14 -0400

    Handle MixDensityFunction

--- src/fabric/26.1/main/java/dev/worldgen/lithostitched/mixin/common/c2me/McToAstMixin.java
+++ src/fabric/26.1/main/java/dev/worldgen/lithostitched/mixin/common/c2me/McToAstMixin.java
@@ -2,6 +2,12 @@ package dev.worldgen.lithostitched.mixin.common.c2me;
 
 import com.ishland.c2me.opts.dfc.common.ast.AstNode;
 import com.ishland.c2me.opts.dfc.common.ast.McToAst;
+import com.ishland.c2me.opts.dfc.common.ast.binary.AddNode;
+import com.ishland.c2me.opts.dfc.common.ast.binary.MaxNode;
+import com.ishland.c2me.opts.dfc.common.ast.binary.MinNode;
+import com.ishland.c2me.opts.dfc.common.ast.binary.MulNode;
+import com.ishland.c2me.opts.dfc.common.ast.misc.ConstantNode;
+import com.ishland.c2me.opts.dfc.common.ast.unary.NegMulNode;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import dev.worldgen.lithostitched.compat.c2me.ast.unary.*;
 import dev.worldgen.lithostitched.impl.worldgen.densityfunction.*;
@@ -27,6 +33,28 @@ public abstract class McToAstMixin {
             case FloorDensityFunction f -> new FloorNode(toAst(f.argument()));
             case SinDensityFunction f -> new SinNode(toAst(f.argument()));
             case SqrtDensityFunction f -> new SqrtNode(toAst(f.argument()));
+
+            // misc
+            case MixDensityFunction f -> {
+                // further optimization is handled in FoldConstants
+                AstNode clampedInput = new MinNode(new ConstantNode(1), new MaxNode(new ConstantNode(0), toAst(f.input())));
+                yield new AddNode(
+                        new MulNode(
+                                toAst(f.argument1()),
+                                new AddNode(
+                                        new ConstantNode(1),
+                                        new MulNode(
+                                                clampedInput,
+                                                new ConstantNode(-1)
+                                        )
+                                )
+                        ),
+                        new MulNode(
+                                toAst(f.argument2()),
+                                clampedInput
+                        )
+                );
+            }
             default -> original;
         };
     }
